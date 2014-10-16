4d0120765c087dbf87f9579444500600de452562
Tristan Carranante <tristan@carranante.name>
2014-10-16 14:44:26 +0200

    exception 2

--- massive/utils.py
+++ massive/utils.py
@@ -13,23 +13,27 @@ def getPageFavicon(url, iconUri=None):
             page = None
 
     if not page:
-        page = urlopen("http://getfavicon.appspot.com/%s" % url, timeout = 5)
-
-    i = BytesIO(page.read())
-
-    if i:
-        image = BytesIO()
         try:
-            Image.open(i).save(image, "PNG")
-            image.seek(0)
-            fav = Favicon()
-            fav.image.put(image)
-            fav.save()
-            return fav
-        except Exception as e:
-            print("error with image")
-    else:
-        return None
+            page = urlopen("http://getfavicon.appspot.com/%s" % url, timeout = 5)
+        except:
+            page = None
+
+    if page:
+        i = BytesIO(page.read())
+
+        if i:
+            image = BytesIO()
+            try:
+                Image.open(i).save(image, "PNG")
+                image.seek(0)
+                fav = Favicon()
+                fav.image.put(image)
+                fav.save()
+                return fav
+            except Exception as e:
+                print("error with image")
+        else:
+            return None
 
 def getPageTitle(url):
         page = urlopen(url)
