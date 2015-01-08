d51a1903485cb4748f69967232edd15cbe18d1a7
Tristan Carranante <tristan@carranante.name>
2015-01-08 20:12:48 +0100

    missing addUser

---
+++ addUser.py
@@ -0,0 +1,17 @@
+"""Import Bookmarks.
+
+Usage:
+    addUser.py (USERNAME)
+"""
+
+from docopt import docopt
+from massive.models import Users
+from massive import bcrypt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    passwd = input("Enter password: ")
+    user = Users(login=arguments['USERNAME'],password=bcrypt.generate_password_hash(passwd))
+    user.save()
+    print("user %s saved" % user.login)
