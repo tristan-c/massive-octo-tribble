5b9f542d890527293a51577e072ef7ad21cb064b
Tristan Carranante <tristan@carranante.name>
2015-01-09 10:49:08 +0100

    add password field

--- massive/forms.py
+++ massive/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     login = StringField('login', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
     #remember_me = BooleanField('remember_me', default=False)
