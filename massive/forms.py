b50f4cae5b1f15d340d41ba132e4fe43b2191f5b
Tristan Carranante <tristan@carranante.name>
2015-01-09 10:51:14 +0100

    add password field

--- massive/forms.py
+++ massive/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
