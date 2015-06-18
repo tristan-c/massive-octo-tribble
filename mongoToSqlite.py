e0cf5753f27b2ac06103f8744db3118a8f93a0f6
Tristan Carranante <tristan@carranante.name>
2015-06-18 16:34:39 +0200

    transfert script done

---
+++ mongoToSqlite.py
@@ -0,0 +1,49 @@
+"""Import Users and links from old massive version using mongoDB.
+
+Usage:
+    mongoToSqlite.py [(<links_path> <users_path>)]
+"""
+
+import json
+
+from docopt import docopt
+from massive import db, models
+from massive.views import save_link
+from pony.orm import db_session
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    list_users = {}
+
+    with open(arguments['<users_path>']) as users_file:
+        for file_line in users_file:
+
+            line = json.loads(file_line)
+
+            with db_session:
+                user = models.Users.get(login=line["login"])
+
+                if not user:
+                    user = models.Users(
+                        login=line["login"],
+                        password=line["password"])
+
+                list_users[line["_id"]["$oid"]] = user
+
+    with open(arguments['<links_path>']) as links_file:
+        for file_line in links_file:
+            
+            line = json.loads(file_line)
+
+            with db_session:
+                save_link(
+                        line["title"],
+                        line["url"],
+                        line["tags"],
+                        None,
+                        list_users[line["user"]["$oid"]]
+                        )
+
+
