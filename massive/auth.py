ebcff18374ff063aa4a1cabe14e5c921c545ac11
Tristan Carranante <tristan@carranante.name>
2015-06-18 17:21:27 +0200

    better login page

--- massive/auth.py
+++ massive/auth.py
@@ -24,23 +24,23 @@ def login():
         return redirect(url_for('index'))
 
     form = LoginForm()
-    error = None
+    error = []
     if form.validate_on_submit():
         try:
             with db_session:
                 user = Users.get(login=form.login.data)
             if not user:
                 raise ValueError("Unkown user")
-        except Exception as error:
-            return render_template("login.html", form=form, error=error)
+        except Exception as e:
+            return render_template("login.html", login_user_form=form, error=[e])
 
         if not bcrypt.check_password_hash(user.password, form.password.data):
-            error = "bad password"
+            error.append("bad password")
         else:
             login_user(user)
             return redirect(request.args.get("next") or url_for("index"))
 
-    return render_template("login.html", form=form, error=error)
+    return render_template("security/login_user.html", login_user_form=form, error=error)
 
 
 @app.route('/logout')
